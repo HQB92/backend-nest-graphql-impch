bf60f025142204934eedf18f61b71cfed1640411
Hugo Esteban Quinteros Bustos <hquinteros@ing.ucsc.cl>
2024-08-03 22:05:19 -0400

    f

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,11 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './local-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
-
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {
-    try {
-      return this.authService.login(req.user);
-    } catch (error) {
-      //res.status(400).send({ message: error.message });
-      return error.message;
-    }
+    return req.user;
   }
 }
