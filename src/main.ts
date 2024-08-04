8fb75a9b644aaf7838c08bac1d5aba80cbad561d
Hugo Esteban Quinteros Bustos <hquinteros@ing.ucsc.cl>
2024-08-03 22:02:40 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
