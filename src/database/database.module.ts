1a93b48b3b39fd5f633888920e9b058b13b3d6cd
Hugo Esteban Quinteros Bustos <hquinteros@ing.ucsc.cl>
2024-08-03 21:29:30 -0400

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,34 +1,31 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from '../models/user.model';
-import * as dotenv from 'dotenv';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Member } from '../models/member.model';
-import { Status } from '../models/status.model';
 import { Church } from '../models/church.model';
+import { Status } from '../models/status.model';
+import * as dotenv from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+
 dotenv.config();
 
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    SequelizeModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        dialect: 'postgres',
-        host: configService.get<string>('PGHOST'),
-        port: 5432,
-        username: configService.get<string>('PGUSER'),
-        password: configService.get<string>('PGPASSWORD'),
-        database: configService.get<string>('PGDATABASE'),
-        autoLoadModels: false,
-        synchronize: false,
-      }),
-      inject: [ConfigService],
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: PGHOST,
+      port: 5432,
+      database: PGDATABASE,
+      username: PGUSER,
+      password: PGPASSWORD,
+      models: [User, Member, Church, Status],
+      autoLoadModels: true,
     }),
-    SequelizeModule.forFeature([User, Member, Status, Church]),
+    SequelizeModule.forFeature([User, Member, Church, Status]),
   ],
-  exports: [SequelizeModule],
 })
 export class DatabaseModule {}
