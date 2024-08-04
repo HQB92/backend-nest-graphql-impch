c194feb858dce6e273c84940ff25f57a715a8783
Hugo Esteban Quinteros Bustos <hquinteros@ing.ucsc.cl>
2024-08-03 21:13:44 -0400

    Update local-auth.guard.ts

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -2,8 +2,8 @@ import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('jwt') {
-  async canActivate(context: ExecutionContext) {
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const result = (await super.canActivate(context)) as boolean;
     const request = context.switchToHttp().getRequest();
     await super.logIn(request);
