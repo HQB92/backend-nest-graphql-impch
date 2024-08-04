96d96b6b4a50d85cac807002974cbddd9e95038e
Hugo Esteban Quinteros Bustos <hquinteros@ing.ucsc.cl>
2024-08-03 21:23:09 -0400

    Update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from '../models/user.model';
-import { Member } from '../models/member.model';
 import * as dotenv from 'dotenv';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Church } from '../models/church.model';
-import { Status } from '../models/status.model';
 dotenv.config();
 
 @Module({
@@ -22,12 +19,12 @@ dotenv.config();
         username: configService.get<string>('PGUSER'),
         password: configService.get<string>('PGPASSWORD'),
         database: configService.get<string>('PGDATABASE'),
-        autoLoadModels: true,
+        autoLoadModels: false,
         synchronize: false,
       }),
       inject: [ConfigService],
     }),
-    SequelizeModule.forFeature([User, Member, Church, Status]),
+    SequelizeModule.forFeature([User]),
   ],
   exports: [SequelizeModule],
 })
